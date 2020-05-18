e9289b62c40b7ff0a0685457d5ac0e76153a00dd
Igor Kagan <igor.b.kagan@gmail.com>
2020-05-18 16:34:49 +0200

    Update chseq_analyze_trial_seq_all_subj_FRD.m

--- chseq_analyze_trial_seq_all_subj_FRD.m
+++ chseq_analyze_trial_seq_all_subj_FRD.m
@@ -4,12 +4,12 @@ data=importdata('Y:\Personal\AlexandraWitt\current_dat_file.mat');
 data=data(data.success==1,:);
 subjects=unique(data.subj);
 
-for i=1:1% length(subjects)
+for i=1:length(subjects)
     subject=subjects(i);
-    subject = 'DAGU';
+    % subject = 'DAGU'; for debigging specific subject
     subjdata=data(data.subj==subject,:);
-    O(i).out = chseq_analyze_trial_seq_one_subj_FRD(subjdata)
-    % set(gcf,'NumberTitle', 'off', 'Name', char(subject))
+    O(i).out = chseq_analyze_trial_seq_one_subj_FRD(subjdata);
+    set(gcf,'NumberTitle', 'off', 'Name', char(subject))
     % set(findall(gcf,'-property','FontSize'),'FontSize',10); % if you want to change all font sizes
 end
 
