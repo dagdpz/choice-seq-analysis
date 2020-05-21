6aeb9ab4e17a9d048044a330e85b60d1ac341374
alexandrawitt <alexandra.witt@gmx.net>
2020-05-21 12:04:49 +0200

    attempt at stacked

--- chseq_analyze_trial_seq_all_subj_FRD.m
+++ chseq_analyze_trial_seq_all_subj_FRD.m
@@ -7,14 +7,14 @@ for i=1:length(subjects)
         compl_subjs=[compl_subjs subjects(i)];
     end
 end
-colormap jet
+
 for i=1:length(compl_subjs)
     subject=compl_subjs(i);
-    subject = 'DAGU'; for debugging specific subject
+%    subject = 'DAGU'; for debugging specific subject
     subjdata=data(data.subj==subject,:);
     O(i).out = chseq_analyze_trial_seq_one_subj_FRD(subjdata);
    
-     set(gcf,'NumberTitle', 'off', 'Name', char(subject))
+%      set(gcf,'NumberTitle', 'off', 'Name', char(subject))
 %      pos= get(gcf, 'Position');
 %      set(gcf,'PaperPositionMode','Auto','PaperType','A4','PaperUnits','points','PaperSize',[pos(3) pos(4)]);
 %      saveas(gcf,[char(subject) '.png'],'png');
@@ -46,6 +46,11 @@ for i=1:length(compl_subjs)
     RFL_Ppc_R(i) = O(i).out.Ppc_LR(3,2);
     RFR_Ppc_L(i) = O(i).out.Ppc_LR(4,1);
     RFR_Ppc_R(i) = O(i).out.Ppc_LR(4,2);
+    
+    Ppc_LR_P_1(i)= O(i).out.Ppc_LR_P(1);
+    Ppc_LR_P_2(i)= O(i).out.Ppc_LR_P(2);
+    Ppc_LR_P_3(i)= O(i).out.Ppc_LR_P(3);
+    Ppc_LR_P_4(i)= O(i).out.Ppc_LR_P(4);
         
     SFL_Pnc_L(i) = O(i).out.Pnc_LR(1,1);
     SFL_Pnc_R(i) = O(i).out.Pnc_LR(1,2);
@@ -55,6 +60,11 @@ for i=1:length(compl_subjs)
     RFL_Pnc_R(i) = O(i).out.Pnc_LR(3,2);
     RFR_Pnc_L(i) = O(i).out.Pnc_LR(4,1);
     RFR_Pnc_R(i) = O(i).out.Pnc_LR(4,2);
+    
+    Pnc_LR_P_1(i)= O(i).out.Pnc_LR_P(1);
+    Pnc_LR_P_2(i)= O(i).out.Pnc_LR_P(2);
+    Pnc_LR_P_3(i)= O(i).out.Pnc_LR_P(3);
+    Pnc_LR_P_4(i)= O(i).out.Pnc_LR_P(4);
     
 end
 
@@ -82,26 +92,73 @@ colormap cool
 % saveas(gcf,['subject_summary.png'],'png');
 
 %comparison of preceeding/next LR grouped
+
+% plotdata=NaN(11,4,2);
+% for i=1:length(compl_subjs)
+%     plotdata(i,1,1)=SFL_Ppc_L(i);
+%     plotdata(i,2,1)=SFR_Ppc_L(i);
+%     plotdata(i,3,1)=RFL_Ppc_L(i);
+%     plotdata(i,4,1)=RFR_Ppc_L(i);
+%     
+%     plotdata(i,1,2)=SFL_Ppc_R(i);
+%     plotdata(i,2,2)=SFR_Ppc_R(i);
+%     plotdata(i,3,2)=RFL_Ppc_R(i);
+%     plotdata(i,4,2)=RFR_Ppc_R(i);
+% end
+    
+sigs=NaN(1,length(compl_subjs)*4);
+
+for i=1:length(compl_subjs)
+    sigs(find(isnan(sigs),1))= Ppc_LR_P_1(i);
+    sigs(find(isnan(sigs),1))=Ppc_LR_P_2(i);
+    sigs(find(isnan(sigs),1))=Ppc_LR_P_3(i);
+    sigs(find(isnan(sigs),1))=Ppc_LR_P_4(i);
+end
+x=[0.7,0.9,1.1,1.3,1.7,1.9,2.1,2.3,2.7,2.9,3.1,3.3,3.7,3.9,4.1,4.3,4.7,4.9,5.1,5.3,5.7,5.9,6.1,6.3,6.7,6.9,7.1,7.3,7.7,7.9,8.1,8.3,8.7,8.9,9.1,9.3,9.7,9.9,10.1,10.3,10.7,10.9,11.1,11.3];
 ig_figure('Position',[100 100 1800 1000],'Name',sprintf('Free Reach Decision choice sequence, LR, %d subjects',length(compl_subjs)));
 
 subplot(2,1,1)
-h1 = bar([SFL_Ppc_L; SFL_Ppc_R; SFR_Ppc_L; SFR_Ppc_R; RFL_Ppc_L; RFL_Ppc_R; RFR_Ppc_L; RFR_Ppc_R]','grouped'); hold on;
-%Ppc_sig = NaN(1,length(compl_subjs)); Ppc_sig(Ppc_LR_P<0.05) = 0.95; plot(Ppc_sig,'k*');
+% h1 = plotBarStackGroups(plotdata,cellstr(compl_subjs),{'SFL','SFR','RFL','RFR'}); hold on;
+% legend({'L' 'R'})
+%h1 = bar([SFL_Ppc_L; SFR_Ppc_L; RFL_Ppc_L;  RFR_Ppc_L]','grouped'); hold on;
+Ppc_sig = NaN(1,length(compl_subjs)*4); Ppc_sig(sigs<0.05) = 0.95; plot(x,Ppc_sig,'k*');
 set(gca,'Xtick',[1:length(compl_subjs)],'XtickLabel',cellstr(compl_subjs));
 title('P(preceding|current)');
 colormap cool
-legend({'SFL_L' 'SFL_R' 'SFR_L' 'SFR_R' 'RFL_L' 'RFL_R' 'RFR_L' 'RFR_R'})
+legend({'SFL' 'SFR' 'RFL' 'RFR'})
 
+% plotdata=NaN(11,4,2);
+% for i=1:length(compl_subjs)
+%     plotdata(i,1,1)=SFL_Pnc_L(i);
+%     plotdata(i,2,1)=SFR_Pnc_L(i);
+%     plotdata(i,3,1)=RFL_Pnc_L(i);
+%     plotdata(i,4,1)=RFR_Pnc_L(i);
+%     
+%     plotdata(i,1,2)=SFL_Pnc_R(i);
+%     plotdata(i,2,2)=SFR_Pnc_R(i);
+%     plotdata(i,3,2)=RFL_Pnc_R(i);
+%     plotdata(i,4,2)=RFR_Pnc_R(i);
+% end
+
+sigs=NaN(1,length(compl_subjs)*4);
+
+for i=1:length(compl_subjs)
+    sigs(find(isnan(sigs),1))= Pnc_LR_P_1(i);
+    sigs(find(isnan(sigs),1))=Pnc_LR_P_2(i);
+    sigs(find(isnan(sigs),1))=Pnc_LR_P_3(i);
+    sigs(find(isnan(sigs),1))=Pnc_LR_P_4(i);
+end
 
 subplot(2,1,2)
-h2 = bar([SFL_Pnc_L; SFL_Pnc_R; SFR_Pnc_L; SFR_Pnc_R; RFL_Pnc_L; RFL_Pnc_R; RFR_Pnc_L; RFR_Pnc_R]','grouped'); hold on;
-%Pnc_sig = NaN(1,length(compl_subjs)); Pnc_sig(Pnc_LR_P<0.05) = 0.95; plot(Pnc_sig,'k*');
+h2 = bar([SFL_Pnc_L; SFR_Pnc_L; RFL_Pnc_L;  RFR_Pnc_L]','grouped'); hold on;
+Pnc_sig = NaN(1,length(compl_subjs)*4); Pnc_sig(sigs<0.05) = 0.95; plot(x,Pnc_sig,'k*');
 set(gca,'Xtick',[1:length(compl_subjs)],'XtickLabel',cellstr(compl_subjs));
 title('P(next|current)');
 colormap cool
+legend({'SFL' 'SFR' 'RFL' 'RFR'})
+
 
 % pos=get(gcf,'Position');
 % set(gcf,'PaperPositionMode','Auto','PaperType','A4','PaperUnits','points','PaperSize',[pos(3) pos(4)]);
 % saveas(gcf,['subject_summary_LR.png'],'png');
-
 
